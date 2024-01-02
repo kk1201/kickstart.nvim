320870ad0926f4dd0491be7058918d86715a2137
kk1201 <kacperkuzma12@gmail.com>
2024-01-02 00:50:33 +0000

    Updating plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,43 +3,44 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    -- Workspaces
-    'natecraddock/workspaces.nvim',
-    'natecraddock/sessions.nvim',
+  -- Workspaces
+  'natecraddock/workspaces.nvim',
+  'natecraddock/sessions.nvim',
 
-    {
-        'nvim-tree/nvim-tree.lua',
-        version = "",
-        lazy = false,
-        dependencies = {
-          "nvim-tree/nvim-web-devicons",
-        },
-        opts = {}
-      },
-    
-      {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        opts = {}
-      },
-    
-      {
-        'rcarriga/nvim-dap-ui',
-        dependencies = 'mfussenegger/nvim-dap',
-        config = function()
-          local dap = require("dap")
-          local dapui = require("dapui")
-          print("hello")
-          dapui.setup()
-          dap.listeners.after.event_initialized["dapui_config"] = function ()
-            dapui.open()
-          end
-          dap.listeners.before.event_terminated["dapui_config"] = function ()
-            dapui.close()
-          end
-          dap.listeners.after.event_exited["dapui_config"] = function ()
-            dapui.close()
-          end
-        end
-      }
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = "",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {}
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {}
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = 'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      print("hello")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.after.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  vim.keymap.set('n', '<leader>so', '<cmd>ObsidianSearch<CR>', { desc = '[S]earch [O]bsidian Note' })
 }
---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,25 @@
+return {
+  "epwalsh/obsidian.nvim",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+
+  opts = {
+    workspaces = {
+      {
+        name = "myNotes",
+        path = "~\\Dropbox\\Dropbox Obsidian"
+      }
+    }
+  },
+  -- mappings = {
+  --   ["gf"] = {
+  --     action = function()
+  --       return require("obsidian").util.gf_passthrough()
+  --     end,
+  --     opts = { noremap = false, expr = true, buffer = true }
+  --   }
+  -- }
+}
